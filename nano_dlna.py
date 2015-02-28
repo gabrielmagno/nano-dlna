aebe16a28622b8240a895d2cac4e9e2099dfb675
Gabriel Magno <gabrielmagno1@gmail.com>
2015-02-28 11:32:32 -0300

    Implemented threading for the http server

--- nano_dlna.py
+++ nano_dlna.py
@@ -1,9 +1,8 @@
+import threading
 import http.server
 import os
 
 class StreamingHTTPRequestHandler(http.server.SimpleHTTPRequestHandler):
-
-    server_version = "StreamingHTTP/0.1"
 
     def translate_path(self, path):
         if self.files_index is None:
@@ -18,13 +17,17 @@ http_port = HTTP_PORT_DEFAULT
 files = {"file_cover": "/dados/Videos/Series/Seinfeld/cover.jpg",
          "file_video": "/dados/Videos/Series/Seinfeld/Pilot - [DVD]/001 - The Seinfeld Chronicles - [DVD].avi",
          "file_subtitle": "/dados/Videos/Series/Seinfeld/Pilot - [DVD]/001 - The Seinfeld Chronicles - [DVD].srt"}
+files_index = {"/{}".format(os.path.basename(file_path)):file_path 
+                   for (file_ref, file_path) in files.items() } 
 
 httph = StreamingHTTPRequestHandler
-httph.files_index = {"/{}".format(os.path.basename(file_path)):file_path 
-                         for (file_ref, file_path) in files.items() } 
+httph.files_index = files_index 
 
 httpd = http.server.HTTPServer(("", http_port), httph)
 
+thread_httpd = threading.Thread(target=httpd.serve_forever, args=[])
+
 print("HTTP server started: http://localhost:{}/".format(http_port))
-httpd.serve_forever()
+thread_httpd.start()
+
 
