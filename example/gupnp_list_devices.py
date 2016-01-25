428222a23662e39e00d65834874f1a14c32f1d29
Gabriel Magno <gabrielmagno1@gmail.com>
2016-01-24 23:19:55 -0200

    Script to list devices with gupnp

---
+++ example/gupnp_list_devices.py
@@ -0,0 +1,11 @@
+from gi.repository import GLib, GUPnP
+
+def device_available (cp, proxy):
+   print ("Found " + proxy.get_friendly_name ())
+
+ctx = GUPnP.Context.new (None, "wlp3s0", 0)
+cp = GUPnP.ControlPoint.new (ctx, "upnp:rootdevice")
+cp.set_active (True)
+cp.connect ("device-proxy-available", device_available)
+
+GLib.MainLoop ().run ()
