0cd970660f40d6665b144a180c68fc26e9770316
Gabriel Magno <gabrielmagno1@gmail.com>
2016-05-20 18:40:46 -0300

    Refatorando

---
+++ REFACTOR/devices.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python3
+# encoding: UTF-8
+
+import socket
+import re
+import urllib.request
+import xml.etree.ElementTree as ET
+
+SSDP_BROADCAST_PORT = 1900
+SSDP_BROADCAST_ADDR = "239.255.255.250"
+
+SSDP_BROADCAST_PARAMS = ["M-SEARCH * HTTP/1.1",
+                         "HOST: {}:{}".format(SSDP_BROADCAST_ADDR,
+                                              SSDP_BROADCAST_PORT),
+                         "MAN: \"ssdp:discover\"",
+                         "MX: 10",
+                         "ST: ssdp:all", "", ""]
+SSDP_BROADCAST_MSG = "\r\n".join(SSDP_BROADCAST_PARAMS)
+
+
+def get_devices(timeout=3.0):
+
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+    s.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, 4)
+    s.bind(("", SSDP_BROADCAST_PORT+10))
+    
+    s.sendto(bytes(SSDP_BROADCAST_MSG, "UTF-8"), (SSDP_BROADCAST_ADDR, 
+                                                  SSDP_BROADCAST_PORT))
+    s.settimeout(timeout)
+    
+    devices = []
+    while True:
+
+        try:
+            data, addr = s.recvfrom(1024)
+        except socket.timeout:
+            break
+
+        try:
+            info = [a.split(":", 1) for a in data.decode("UTF-8").split("\r\n")[1:]]
+            device = dict([(a[0].strip().lower(), a[1].strip()) for a in info if len(a) >= 2])
+            devices.append(device)
+        except:
+            pass
+
+    devices = [device for device in devices if "AVTransport" in device["st"]]
+
+    for i in range(len(devices)):
+        location = devices[i]["location"]
+        xml = urllib.request.urlopen(location).read().decode("UTF-8")
+        xml = re.sub(" xmlns=\"[^\"]+\"", "", xml, count=1)
+        info = ET.fromstring(xml)
+        devices[i]["info"] = info
+
+    return devices
+
+
+if __name__ == "__main__":
+
+    devices = get_devices()
+    print(devices)
+
