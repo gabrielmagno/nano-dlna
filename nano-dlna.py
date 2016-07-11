1c670524cfb87f986d9115efda8bcb289b6caae6
Gabriel Magno <gabrielmagno1@gmail.com>
2016-07-11 18:11:05 -0300

    Implemented CLI

--- nano-dlna.py
+++ nano-dlna.py
@@ -1,29 +1,97 @@
+#!/usr/bin/env python3
+
+import sys
+import os
+import argparse
+
 import devices
 import streaming
 import dlna
 
 
-if __name__ == "__main__":
+def get_subtitle(file_video):
 
-    import sys
+    video, extension = os.path.splitext(file_video)
 
-    device_query = sys.argv[1]
-    file_video = sys.argv[2]
-    file_subtitle = sys.argv[3]
+    file_subtitle = "{}.srt".format(video)
 
-    files = {"file_video":file_video, "file_subtitle":file_subtitle}
-    #files = {"file_video":file_video}
+    if os.path.exists(file_subtitle):
+        return file_subtitle
+    else:
+        return None
     
-    devices = devices.get_devices(5)
-    print(devices)
 
-    device = [device for device in devices if device_query in str(device).lower()][0]
-    
+def list_devices(args):
+
+    my_devices = devices.get_devices(args.timeout)
+
+    for i, device in enumerate(my_devices, 1):
+        device["info"] = "..."
+        print("Device {}:\n{}\n\n".format(i, json.dumps(device, indent=4)))
+
+
+def play(args):
+
+    # Get video and subtitle file names
+
+    files = { "file_video" : args.file_video }
+
+    if args.use_subtitle:
+
+        if not args.file_subtitle:
+            args.file_subtitle = get_subtitle(args.file_video)
+
+        if args.file_subtitle:
+            files["file_subtitle"] = args.file_subtitle
+
+
+    # Select device to play
+
+    my_devices = devices.get_devices(args.timeout)
+
+    if len(my_devices) > 0:
+        if len(args.device_query) > 0:
+            device = [ device for device in my_devices 
+                           if args.device_query in str(device).lower() ][0]
+        else:
+            device = my_devices[0]
+    else:
+        sys.exit("No devices found.")
+
+
+    # Configure streaming server
+
     target_ip = device["hostname"]
     serve_ip = streaming.get_serve_ip(target_ip)
     files_urls = streaming.start_server(files, serve_ip)
-    #files_urls = {key:value.replace("9000", "3000") for key, value in files_urls.items()}
-    print(files_urls)
+
+
+    # Play the video through DLNA protocol 
 
     dlna.play(files_urls, device)
+
+
+def run():
+
+    parser = argparse.ArgumentParser(description="A minimal UPnP/DLNA media streamer.")
+    parser.add_argument("-t", "--timeout", type=float, default=5)
+    subparsers = parser.add_subparsers(dest="subparser_name")
+
+    p_list = subparsers.add_parser('list')
+    p_list.set_defaults(func=list_devices)
+
+    p_play = subparsers.add_parser('play')
+    p_play.add_argument("-d", "--device", dest="device_query")
+    p_play.add_argument("-s", "--subtitle", dest="file_subtitle")
+    p_play.add_argument("-n", "--no-subtitle", dest="use_subtitle", action="store_false")
+    p_play.add_argument("file_video")
+    p_play.set_defaults(func=play)
+
+    args = parser.parse_args()
+    args.func(args)
+
+
+if __name__ == "__main__":
+
+    run()
 
