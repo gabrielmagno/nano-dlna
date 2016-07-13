ad826d4e68571d3012d549d5854cb47404a83560
Gabriel Magno <gabrielmagno1@gmail.com>
2016-07-13 18:05:59 -0300

    Updated setup with classifiers

--- setup.py
+++ setup.py
@@ -7,9 +7,6 @@ install_requires = [
     'Twisted>=16.2.0',
 ]
 
-if sys.version_info < (2, 7):
-    install_requires.append('simplejson>=3.6.3')
-
 setup(
     name='nanodlna',
     version='0.1.0',
@@ -19,27 +16,26 @@ setup(
     author_email='gabrielmagno1@gmail.com',
     url='https://github.com/gabrielmagno/nano-dlna',
     license='MIT',
-    #TODO
-    #classifiers=[
-    #    'Development Status :: 5 - Production/Stable',
-    #    'Environment :: Console',
-    #    'Intended Audience :: Developers',
-    #    'Intended Audience :: End Users/Desktop',
-    #    'Intended Audience :: Science/Research',
-    #    'License :: OSI Approved :: MIT License',
-    #    'Natural Language :: English',
-    #    'Operating System :: OS Independent',
-    #    'Programming Language :: Python',
-    #    'Programming Language :: Python :: 2.7',
-    #    'Programming Language :: Python :: 3.3',
-    #    'Programming Language :: Python :: 3.4',
-    #    'Programming Language :: Python :: 3.5',
-    #    'Programming Language :: Python :: Implementation :: CPython',
-    #    'Programming Language :: Python :: Implementation :: PyPy',
-    #    'Topic :: Scientific/Engineering :: Information Analysis',
-    #    'Topic :: Software Development :: Libraries :: Python Modules',
-    #    'Topic :: Utilities'
-    #],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Intended Audience :: End Users/Desktop',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+	'Programming Language :: Python :: 3.5',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Multimedia :: Sound/Audio',
+        'Topic :: Multimedia :: Video',
+        'Topic :: Utilities'
+    ],
     packages=['nanodlna'],
     package_dir={'nanodlna': 'nanodlna'},
     package_data={'nanodlna': ['templates/*.xml']},
