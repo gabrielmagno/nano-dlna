8a995f759d02216b33e8056d7c6d528b6930fa4a
Your Name <you@example.com>
2016-07-14 00:48:30 -0300

    Bump version

--- nanodlna/__init__.py
+++ nanodlna/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 __title__ = 'nanodlna'
-__version__ = '0.1.0'
+__version__ = '0.1.1'
 __short_version__ = '.'.join(__version__.split('.')[:2])
 __author__ = 'Gabriel Magno'
 __license__ = 'MIT'
--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ install_requires = [
 
 setup(
     name='nanodlna',
-    version='0.1.0',
+    version='0.1.1',
     description='A minimal UPnP/DLNA media streamer',
     long_description='nano-dlna is a command line tool that allows you to play a local video file in your TV (or any other DLNA compatible device)',
     author='Gabriel Magno',
