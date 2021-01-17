340d8139b4692f240fa73eeccaa5dbe1d5c82181
Gabriel Magno <gabrielmagno1@gmail.com>
2021-01-17 12:18:34 -0300

    Fix number of parameters considering new version of gupnp library

--- alternatives/gupnp_list_devices.py
+++ alternatives/gupnp_list_devices.py
@@ -5,7 +5,8 @@ def device_available(cp, proxy):
     print("Found " + proxy.get_friendly_name())
 
 
-ctx = GUPnP.Context.new(None, "wlp3s0", 0)
+#ctx = GUPnP.Context.new(None, "wlp3s0", 0)
+ctx = GUPnP.Context.new("wlp3s0", 0)
 cp = GUPnP.ControlPoint.new(ctx, "upnp:rootdevice")
 cp.set_active(True)
 cp.connect("device-proxy-available", device_available)
